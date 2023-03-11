38e2f114343053af5f06ab4288fd6f12b6c9432f
Helena Recasens <me@helena.re>
2023-03-11 14:00:59 +0100

    volatile cleanup

--- constants.h
+++ constants.h
@@ -1,13 +1,15 @@
+#define __register(pointer) ((volatile uint32_t *) (pointer))
+
 #define GPIOA_BASE 0x40020000
 #define GPIOD_BASE 0x40020C00
 
-#define GPIOx_MODER(base) (base + 0x0)
-#define GPIOx_PUPDR(base) (base + 0x0C)
-#define GPIOx_IDR(base) (base + 0x10)
-#define GPIOx_BSRR(base) (base + 0x18)
+#define GPIOx_MODER(base) __register(base + 0x0)
+#define GPIOx_PUPDR(base) __register(base + 0x0C)
+#define GPIOx_IDR(base) __register(base + 0x10)
+#define GPIOx_BSRR(base) __register(base + 0x18)
 
 #define RCC_BASE 0x40023800
-#define RCC_AHB1ENR (RCC_BASE + 0x30)
+#define RCC_AHB1ENR __register(RCC_BASE + 0x30)
 
 #define MY_VARIABLE ((0x1C000)/2 + 0)
 #define MEM_END (0x20000000 + 0x1C000)
--- main.c
+++ main.c
@@ -5,13 +5,12 @@ extern uint32_t LED_SEQUENCE;
 
 void _start() {
     // Clock stuff
-    volatile uint32_t *rccPtr = (volatile uint32_t *)(RCC_AHB1ENR);
-    *rccPtr |= 1 << 3 | 1 << 0;
+    *RCC_AHB1ENR |= 1 << 3 | 1 << 0;
 
     // Set GPIOA0 to input
-    setRegisterBits((volatile uint32_t *)GPIOx_MODER(GPIOA_BASE), 0, 2, 0b00);
+    setRegisterBits(GPIOx_MODER(GPIOA_BASE), 0, 2, 0b00);
     // Set GPIOA0 to pull-down
-    setRegisterBits((volatile uint32_t *)GPIOx_PUPDR(GPIOA_BASE), 0, 2, 0b10);
+    setRegisterBits(GPIOx_PUPDR(GPIOA_BASE), 0, 2, 0b10);
 
     for (uint32_t *ledPtr = &LED_SEQUENCE; *ledPtr; ledPtr += 2) {
         // Set GPIODX to output
@@ -21,11 +20,11 @@ void _start() {
     uint32_t *ledPtr = &LED_SEQUENCE;
     uint8_t flag = 0;
     while (1) {
-        uint32_t btnDown = getRegisterBits((volatile uint32_t *)GPIOx_IDR(GPIOA_BASE), 0, 1);
+        uint32_t btnDown = getRegisterBits(GPIOx_IDR(GPIOA_BASE), 0, 1);
         if (btnDown) {
             if (!flag) {
                 flag = 1;
-                *(volatile uint32_t *)GPIOx_BSRR(ledPtr[0]) = 0b1 << ledPtr[1];
+                *GPIOx_BSRR(ledPtr[0]) = 0b1 << ledPtr[1];
                 ledPtr += 2;
                 if (!ledPtr[0]) {
                     ledPtr = &LED_SEQUENCE;
@@ -37,7 +36,7 @@ void _start() {
                 }
                 flag = 0;
                 for (uint32_t *firstLedPtr = &LED_SEQUENCE + 1; *firstLedPtr; firstLedPtr += 2) {
-                    *(volatile uint32_t *)GPIOx_BSRR(GPIOD_BASE) = 0b1 << ((*firstLedPtr) + 16);
+                    *GPIOx_BSRR(GPIOD_BASE) = 0b1 << ((*firstLedPtr) + 16);
                 }
             }
         }
