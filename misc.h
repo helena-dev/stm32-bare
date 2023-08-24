8073f3a1e440f7c312cb3828c0eef9e000a46acc
Helena Recasens <me@helena.re>
2023-08-24 20:56:33 +0200

    fix typing

--- misc.h
+++ misc.h
@@ -21,27 +21,27 @@ void setRegisterBits(volatile uint32_t *address, uint8_t start, uint8_t length, 
 
 /**
 :param i32 base: GPIO port base
-:param i5 pin: pin number
-:param i8 pullConfig: PUPDR config
+:param i4 pin: pin number
+:param i2 pullConfig: PUPDR config
 **/
 void gpioSetInput(volatile uint32_t *base, uint8_t pin, uint8_t pullConfig);
 
 /**
 :param i32 base: GPIO port base
-:param i5 pin: pin number
+:param i4 pin: pin number
 **/
 void gpioSetOutput(volatile uint32_t *base, uint8_t pin);
 
 /**
 :param i32 base: GPIO port base
-:param i5 pin: pin number
+:param i4 pin: pin number
 :return bool: pin state
 **/
 bool gpioRead(volatile uint32_t *base, uint8_t pin);
 
 /**
 :param i32 base: GPIO port base
-:param i5 pin: pin number
+:param i4 pin: pin number
 :param bool value: new pin state
 **/
 void gpioWrite(volatile uint32_t *base, uint8_t pin, bool value);
